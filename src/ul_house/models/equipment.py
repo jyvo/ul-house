45fe77234d060cb039c93688acfa3aea613d7a8f
Joseph Zhang <jzhang.joe03@gmail.com>
2026-09-18 22:48:20 -0400

    refactor: update kw_only clause for equipment dataclasses

--- src/ul_house/models/equipment.py
+++ src/ul_house/models/equipment.py
@@ -5,18 +5,23 @@ from abc import ABC
 from ul_house.models.combat_mechanism import Element
 from ul_house.models.gear_mechanism import Proc, WeaponAbility, MonsterSkill, PassiveSkill, HiddenPotential
 from ul_house.models.gear_evolution import Reforge, Awakening, Enlightening
-
+from ul_house.config import BASE_URL
 
 @dataclass(frozen=True, slots=True)
 class ItemRef:
     id: str
-    icon_url: str
     name: str
+
+    @property
+    def icon_url(self) -> str:
+        return f"{BASE_URL}/images/equipicon/{self.id}.png"
 
 
 @dataclass(frozen=True, slots=True)
 class EquipmentRef(ItemRef):
-    info_url: str
+    @property
+    def info_url(self) -> str:
+        return f"{BASE_URL}/en/equip_detail/{self.id}.html"
 
 
 @dataclass(frozen=True, slots=True)
@@ -24,38 +29,46 @@ class Equipment(EquipmentRef, ABC):
     rarity: str
     gear_type: str
     cost: int
-    element: Element | None
+    element: Element
     max_level: int
     stats: tuple[Stats, ...]
-    reforge: Reforge
-    awakening: Awakening | None
+    reforge: Reforge | None = None # None if before and after are not present via parser, note table always present
+    awakening: Awakening | None = None
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass(frozen=True, slots=True, kw_only=True)
 class Weapon(Equipment):
-    infusion_count: int
+    infusion_count: int = 0
     skill: Proc
-    weapon_ability: WeaponAbility | None
+    weapon_ability: WeaponAbility | None = None
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass(frozen=True, slots=True, kw_only=True)
 class DefensiveGear(Equipment):
-    infusion_count: int
+    infusion_count: int = 0
     skill: Proc
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass(frozen=True, slots=True, kw_only=True)
 class Monster(Equipment):
-    hidden_potential_count: int
-    skill: tuple[MonsterSkill, MonsterSkill | None]
-    passive: PassiveSkill | None
-    hidden_potential: HiddenPotential | None
-    enlightening: Enlightening | None
+    skill: tuple[MonsterSkill, ...]
+    passive: PassiveSkill | None = None
+    hidden_potential: HiddenPotential | None = None
+    enlightening: Enlightening | None = None
+
+    @property
+    def hidden_potential_count(self) -> int:
+        return self.hidden_potential.max_level if self.hidden_potential is not None else 0
 
 
 @dataclass(frozen=True, slots=True)
 class Stats:
     label: str      # stat label/name
-    tier: str       # initial, max1, max2
-    value: int      # value w.r. to tier
-    slotted: bool   # whether label is set or not
+    values: tuple[tuple[str, int], ...]       # tier: value, e.g. ("initial", 100)
+
+    # change slotted as a property call
+    @property
+    def slotted(self) -> bool:
+        return "stat" not in self.label.casefold()
+
+    
